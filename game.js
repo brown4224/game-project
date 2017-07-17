0b42e6516d5dd950c4d3dd2db4c2051adcaf7dfa
Nikc <nikcdc@gmail.com>
2017-07-17 15:49:59 -0400

    Car jump "animation"
    The car rotates on the x axis when you jump!

--- game.js
+++ game.js
@@ -50,6 +50,9 @@ var projectileArray = [];
 var projectileSpeed = 10 * defaultSpeed;
 var projectileMatrix = [];
 
+//Sound
+var soundArray = [];
+
 
 // Rotate  Variables
 var xAxis = 0; var yAxis = 0; var zAxis = 0;  // Global
@@ -61,6 +64,7 @@ var near = 0.001;
 var far = 30.0;
 var radius = 10.0;
 var theta = 0;  // Radians
+var car_theta = 0; // Car's current movement
 var phi = 0.0;
 var dr = Math.PI / 180.0;
 
@@ -87,7 +91,7 @@ var nightTimer = 10000;  //  Timer Callback,  10 Seconds
 
 // Lighting
 var lightPosition = vec4(5.0, 0.0, 10.0, 0.0);
-var carLightPosition = vec4(-radius * Math.sin(theta), 0.0, -radius * Math.cos(theta), 0.0);  // Points away from car
+var carLightPosition = vec4(-radius * Math.sin(car_theta), 0.0, -radius * Math.cos(car_theta), 0.0);  // Points away from car
 var ambientColor, diffuseColor, specularColor;
 
 
@@ -190,6 +194,10 @@ window.onload = function init() {
     /////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////
     /////////////////////////////////////////////////////////////////////////////////
+    
+    
+    ///////////////////// LOAD SOUNDS ///////////////////////////////
+    soundArray.push(document.getElementById("laserSound"));
 
 
     ///////////////  COLOR BUFFER   //////////////////////
@@ -343,9 +351,12 @@ window.onload = function init() {
         movementMatrix[1] -= y_speed;
         y_speed -= gravity;
         
+        hxAxis -= y_speed * 10;
+        
         if(movementMatrix[1] >= 0.0 && y_speed < 0) {
             y_speed = 0;
             movementMatrix[1] = 0;
+            hxAxis = 0;
             clearInterval(gravity_callback);
         }
     }
@@ -406,10 +417,9 @@ window.onload = function init() {
             speed += acceleration;
         }
         // Calculate futer location on user input
-        var futureX =   speed * Math.sin(theta);
+        var futureX =   speed * Math.sin(car_theta);
         var futureY =   0;
-        var futureZ =   speed *  Math.cos(theta);
-
+        var futureZ =   speed *  Math.cos(car_theta);
 
         var rotationSpeed = 50 * speed;
 
@@ -439,6 +449,15 @@ window.onload = function init() {
             if(!collision){
                 move(1);
             }
+            
+            if(car_theta != theta){
+            if (car_theta > theta){
+                car_theta -= 0.1;
+            }
+            else{
+                car_theta += 0.1;
+            }
+            }
         }
 
         function downArrow(speedAdjust) {
@@ -448,6 +467,15 @@ window.onload = function init() {
             if(!collision){
                 move(-1);
             }
+            
+            if(car_theta != theta){
+            if (car_theta > theta){
+                car_theta -= 0.1;
+            }
+            else{
+                car_theta += 0.1;
+            }
+            }
         }
         function leftArrow(degreeTurn) {
             theta += (dr * degreeTurn);
@@ -455,7 +483,6 @@ window.onload = function init() {
         }
         function rightArrow(degreeTurn) {
             theta -= (dr * degreeTurn);
-
         }
 
         function move(direction) {
@@ -463,16 +490,16 @@ window.onload = function init() {
             movementMatrix[1] += direction * futureY;
             movementMatrix[2] += direction * futureZ;
             // Object Rotation
-            hzAxis += direction * rotationSpeed *  Math.sin(theta);
-            hxAxis += direction * rotationSpeed *  Math.cos(theta);
-            
-            
+            //hzAxis += direction * rotationSpeed *  Math.sin(theta);
         }
 
     }
 
 
     function fire() {
+        
+        soundArray[0].play();
+        
         projectileArray.push( new bullet(2, projectileArray.length ));
         var b = projectileArray[projectileArray.length -1];
         b.callback = setInterval(function () {
